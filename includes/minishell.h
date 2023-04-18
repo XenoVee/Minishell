10b76ac6533084f365ea290ac7f7475c464c1edf
Owen Hearn <ohearn@f1r5s17.codam.nl>
2023-04-18 17:46:04 +0200

    Starting out on Minishell

---
+++ includes/minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   minishell.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ohearn <ohearn@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/04/18 17:43:14 by ohearn        #+#    #+#                 */
+/*   Updated: 2023/04/18 17:45:15 by ohearn        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+/*Defines*/
+# define ERR_MALLOC		"Malloc failed. Please try again."
+/*Structs*/
+/*Functions*/
+
+#endif
