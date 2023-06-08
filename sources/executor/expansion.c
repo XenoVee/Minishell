3c5019367ef24a0ff0a9c1ad631e1bd1c3f86d08
rmaes <ikbenruudmaes@gmail.com>
2023-06-08 14:25:36 +0200

    bbb

---
+++ sources/executor/expansion.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   expansion.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rmaes <rmaes@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/06/05 15:04:06 by rmaes         #+#    #+#                 */
+/*   Updated: 2023/06/08 14:25:24 by rmaes         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include <stdio.h>
+
+static int	ft_explen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '=')
+		i++;
+	return (i);
+}
+
+char	*envsearch(char **envp, char *var)
+{
+	unsigned int	i;
+	unsigned int	explen;
+	unsigned int	strlen;
+
+	i = 0;
+	strlen = ft_strlen(var);
+	while (envp[i])
+	{
+		explen = ft_explen(envp[i]);
+		if (strlen == explen)
+			if (ft_strncmp(envp[i], var, ft_max(explen, strlen)) == 0)
+				return (&envp[i][strlen + 1]);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*expand(char **envp, char *var)
+{
+	char	*ret;
+
+	ret = envsearch(envp, var);
+	if (ret == NULL)
+		return (NULL);
+	return (ret[ft_strlen(var)]);
+}
