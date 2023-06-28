453363f79c8624af1274026c2c072dd4de284b6e
rmaes <ikbenruudmaes@gmail.com>
2023-06-28 16:51:37 +0200

    oops there goes main

--- sources/main/main.c
+++ sources/main/main.c
@@ -6,7 +6,7 @@
 /*   By: ohearn <ohearn@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/18 17:40:12 by ohearn        #+#    #+#                 */
-/*   Updated: 2023/06/27 16:12:45 by rmaes         ########   odam.nl         */
+/*   Updated: 2023/06/28 16:51:02 by rmaes         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,54 +19,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include "builtins.h"
-
-t_commands	*cmdmakewcl(void)
-{
-	t_commands	*cmd;
-	char		**array;
-
-	cmd = malloc(sizeof(t_commands));
-	array = malloc(sizeof(char *) * 3);
-	array[0] = ft_strdup("wc");
-	array[1] = ft_strdup("-l");
-	array[2] = NULL;
-	cmd->args = array;
-	cmd->next = NULL;
-	cmd->prev = NULL;
-	return (cmd);
-}
-
-t_commands	*cmdmakegrep(void)
-{
-	t_commands	*cmd;
-	char		**array;
-
-	cmd = malloc(sizeof(t_commands));
-	array = malloc(sizeof(char *) * 3);
-	array[0] = ft_strdup("grep");
-	array[1] = ft_strdup("i");
-	array[2] = NULL;
-	cmd->args = array;
-	cmd->next = NULL;
-	cmd->prev = NULL;
-	return (cmd);
-}
-
-t_commands	*cmdmakels(void)
-{
-	t_commands	*cmd;
-	char		**array;
-
-	cmd = malloc(sizeof(t_commands));
-	array = malloc(sizeof(char *) * 3);
-	array[0] = ft_strdup("ls");
-	array[1] = ft_strdup("-FAG");
-	array[2] = NULL;
-	cmd->args = array;
-	cmd->next = NULL;
-	cmd->prev = NULL;
-	return (cmd);
-}
 
 t_commands	*cmdmakeenv(void)
 {
@@ -83,18 +35,15 @@ t_commands	*cmdmakeenv(void)
 	return (cmd);
 }
 
-t_commands	*cmdmakeecho(void)
+t_commands	*cmdmakeunset(void)
 {
 	t_commands	*cmd;
 	char		**array;
 
 	cmd = malloc(sizeof(t_commands));
 	array = malloc(sizeof(char *) * 3);
-	array[0] = ft_strdup("echo");
-	array[1] = ft_strdup("-n -n -n -n some words");
-	// array[2] = ft_strdup("-m");
-	// array[3] = ft_strdup("two");
-	// array[4] = ft_strdup("things");
+	array[0] = ft_strdup("unset");
+	array[1] = ft_strdup("TEST");
 	array[2] = NULL;
 	cmd->args = array;
 	cmd->next = NULL;
@@ -102,31 +51,81 @@ t_commands	*cmdmakeecho(void)
 	return (cmd);
 }
 
+t_commands	*cmdmakeexport(void)
+{
+	t_commands	*cmd;
+	char		**array;
+
+	cmd = malloc(sizeof(t_commands));
+	array = malloc(sizeof(char *) * 4);
+	array[0] = ft_strdup("export");
+	array[1] = ft_strdup("TEST=var");
+	array[2] = ft_strdup("VAR=test");
+	array[3] = NULL;
+	cmd->args = array;
+	cmd->next = NULL;
+	cmd->prev = NULL;
+	return (cmd);
+}
+
+t_commands	*cmdmakels(void)
+{
+	t_commands	*cmd;
+	char		**array;
+
+	cmd = malloc(sizeof(t_commands));
+	array = malloc(sizeof(char *) * 3);
+	array[0] = ft_strdup("ls");
+	array[1] = ft_strdup("-FGA");
+	array[2] = NULL;
+	cmd->args = array;
+	cmd->next = NULL;
+	cmd->prev = NULL;
+	return (cmd);
+}
+
+void	leaks(void)
+{
+	system("leaks -q minishell");
+}
+
+void	cmdclear(t_commands *cmd)
+{
+	int	i;
+
+	i = 0;
+	while (cmd->args[i])
+	{
+		free (cmd->args[i]);
+		i++;
+	}
+	free (cmd->args);
+	free (cmd);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	t_dllist	*env;
 	t_commands	*cmd;
+	t_commands	*cmd2;
+	t_commands	*cmd3;
 
-	cmd = cmdmakeecho();
+	// atexit(leaks);
 	env = envcpy(envp);
+	cmd = cmdmakeexport();
+	cmd2 = cmdmakeenv();
+	cmd3 = cmdmakeunset();
+	cmd->next = cmd2;
+	cmd2->prev = cmd;
 	executor(cmd, env);
-	// printf("%s\n", ft_getenv(env, "USER"));
 	argc++;
 	if (argv[0])
 		;
+	cdl_listclear(env);
+	cmdclear(cmd);
+	cmdclear(cmd2);
+	cmdclear(cmd3);
 }
-
-// int yes(void)
-// {
-// 	printf("yes\n");
-// 	return (1);
-// }
-
-// int no(void)
-// {
-// 	printf("no\n");
-// 	return (0);
-// }
 
 // int	main(void)
 // {
